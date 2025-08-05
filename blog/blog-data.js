beb3f301b137fd7e1a6921b74b99794a9b036ed0
jiahuiChen <jhChenofficial@outlook.com>
2025-08-05 16:34:30 +0800

    加入了新的博客

--- blog/blog-data.js
+++ blog/blog-data.js
@@ -1,21 +1,24 @@
 // 博客文章数据配置
 // 添加新文章时，请在这里添加文章信息，主页会自动更新
+// 文章按时间从新到旧排列
 const blogData = [
     {
-        id: 'post1',
-        title: '深度学习在电极微环境识别中的应用',
-        date: '2024年1月15日',
-        category: '科研心得',
-        categoryClass: 'research',
-        excerpt: '本文介绍了如何使用CNN卷积神经网络来识别电极环境中的PTFE纤维和活性颗粒，分享了在毕业论文设计过程中的技术实现和经验总结。',
-        image: 'blog/images/post1.jpg', // 可选，如果没有图片可以不设置
-        tags: ['深度学习', '计算机视觉', '电极材料'],
-        featured: true // 是否在主页显示
+        id: 'post4',
+        title: 'Hell let loose的智能驾驶路线规划',
+        date: '2025年8月5日',
+        dateSort: '2025-08-05',
+        category: '技术分享',
+        categoryClass: 'tech',
+        excerpt: '本文介绍了人间地狱的地图交互式驾驶路线只能规划网站，为用户提供了方便的赛前驾驶路线规划',
+        image: 'blog/images/post4.png',
+        tags: ['Hell let loose', '智能交互', '路线规划'],
+        featured: true
     },
     {
         id: 'post2',
         title: 'Hell let loose的地图战术分析网站',
         date: '2025年8月3日',
+        dateSort: '2025-08-03',
         category: '技术分享',
         categoryClass: 'tech',
         excerpt: '分享人间地狱的赛前地图部署工具',
@@ -27,33 +30,53 @@ const blogData = [
         id: 'post3',
         title: 'Hell let loose的音效库网站',
         date: '2025年8月3日',
+        dateSort: '2025-08-03',
         category: '技术分享',
         categoryClass: 'tech',
         excerpt: '本文介绍了人间地狱的音效库，并为用户熟悉音效提供了测试训练',
         image: 'blog/images/post3.png',
         tags: ['Hell let loose', '音效库'],
         featured: true
+    },
+    {
+        id: 'post1',
+        title: '深度学习在电极微环境识别中的应用',
+        date: '2024年1月15日',
+        dateSort: '2024-01-15',
+        category: '科研心得',
+        categoryClass: 'research',
+        excerpt: '本文介绍了如何使用CNN卷积神经网络来识别电极环境中的PTFE纤维和活性颗粒，分享了在毕业论文设计过程中的技术实现和经验总结。',
+        image: 'blog/images/post1.jpg', // 可选，如果没有图片可以不设置
+        tags: ['深度学习', '计算机视觉', '电极材料'],
+        featured: true // 是否在主页显示
     }
 ];
 
 // 获取特色文章（在主页显示的文章）
 function getFeaturedPosts() {
-    return blogData.filter(post => post.featured).slice(0, 3);
+    return blogData
+        .filter(post => post.featured)
+        .sort((a, b) => new Date(b.dateSort) - new Date(a.dateSort))
+        .slice(0, 9);
 }
 
 // 获取所有文章
 function getAllPosts() {
-    return blogData;
+    return blogData.sort((a, b) => new Date(b.dateSort) - new Date(a.dateSort));
 }
 
 // 根据分类获取文章
 function getPostsByCategory(category) {
-    return blogData.filter(post => post.categoryClass === category);
+    return blogData
+        .filter(post => post.categoryClass === category)
+        .sort((a, b) => new Date(b.dateSort) - new Date(a.dateSort));
 }
 
 // 获取最新文章
 function getLatestPosts(count = 3) {
-    return blogData.slice(0, count);
+    return blogData
+        .sort((a, b) => new Date(b.dateSort) - new Date(a.dateSort))
+        .slice(0, count);
 }
 
 // 导出数据（如果在Node.js环境中使用）
---
+++ blog/post_md/post4.md
@@ -0,0 +1,44 @@
+# 人间地狱交互式驾驶路线
+
+**发布日期：** 2025年8月5日  
+**作者：** 陈家辉  
+**分类：** 技术分享
+**标签：** Hell let loose, 驾驶路线
+
+## 摘要
+
+本网站[https://drive.yuanxunfang.top/](https://drive.yuanxunfang.top/) 主要提供了《人间地狱》这款游戏不同地图的交互式驾驶路线生成，仅需在地图上点击即可直接生成驾驶路线，支持国内访问加速。
+
+## 项目来源
+
+项目来自于[https://worldwarways.web.app/](https://worldwarways.web.app/)，本网站对于该项目进行了二次开发，提供了中文的友好界面。。更多信息可以参见我的github项目源码，[https://github.com/chenjiahui-offical/HLL_drive.git](https://github.com/chenjiahui-offical/HLL_drive.git)
+
+## 网站界面
+![网页界面截图](../post_images/post4/1.png)
+当前的左上角为菜单栏，进入网站后默认地图为卡朗唐。
+
+### 菜单界面 
+
+![菜单界面截图](../post_images/post4/2.png)
+> 菜单栏点击地图名即可选择不同地图，（开发中）标明当前该地图仅提供查看功能，不支持路线规划
+
+### 路线规划
+![路线规划截图](../post_images/post4/3.png)
+> 按住地图即可实现地图拖拽。单击地图会生成坐标点，默认以第一个坐标点为起始点，最后一个坐标点为终点。
+
+![功能按钮截图](../post_images/post4/4.png)
+> 确定好坐标点后，点击正下方中间的三角标即可开始规划路线，系统会自动生成从起点到终点的路线，确保依次经过每一个坐标点。点击正下方左侧的撤销键，即可退回上一步。点击正下方右侧的清除键，即可一键清空地图。
+
+## 总结
+
+本文主要介绍了[https://drive.yuanxunfang.top/](https://drive.yuanxunfang.top/) 该网站，即HLL交互式驾驶路线，支持提供不同地图的驾驶路线智能规划，望人间地狱CN社区更进一步！。
+
+## 打赏
+![微信支付截图](../post_images/post4/5.png)
+> 汉化+二次开发不易，CDN+网站为自费搭建，只为国内用户能有友好的访问体验，打赏自愿
+---
+
+**导航：**
+- [← 上一篇](post3.html)
+- [返回博客列表](index.html)
+- [下一篇 →](post5.html)
